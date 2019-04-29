d2285ac17897be4f0e3303601152a4894686636b
chonlatis <50080848+chonlatis1993@users.noreply.github.com>
2019-04-29 11:12:41 +0700

    Add files via upload

--- firstbot.php
+++ firstbot.php
@@ -35,7 +35,7 @@ if (!is_null($events['events'])) //check ค่าในตัวแปร $even
             $txtin = $event['message']['text'];//เอาข้อความจากไลน์ใส่ตัวแปร $txtin
             if($txtin == 'โหลๆ')
             {
-                $txtback = '5 โหล'
+                $txtback = '5 โหล';
             }
             reply_msg($txtback,$replyToken);      
         }
