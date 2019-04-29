3725136569252e9c5dd2ec0f8f63c466731ccba5
chonlatis <chonlatis1993@gmail.com>
2019-04-29 16:57:21 +0700

    ekrgbegehg

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?php
     require('connect_db.php');
-    $sql_text = "SELECT * FROM linebot WHERE keyword LIKE '%b%'";
+    $sql_text = "SELECT * FROM linebot WHERE keyword LIKE '%a%'";
     $query = mysqli_query($conn,$sql_text);
     while($objresult = mysqli_fetch_assoc($query))
     {
