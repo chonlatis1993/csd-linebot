ef7a076c6a7db6cf0ae1c96f88a8580e5b8e3642
chonlatis <chonlatis1993@gmail.com>
2019-04-30 15:42:07 +0700

    aa

--- query.php
+++ query.php
@@ -1,5 +1,11 @@
 <?php
-    require('../connect_db.php');
+    $host = 'us-cdbr-iron-east-01.cleardb.net';
+    $user = 'b859a12ba0c2d9';
+    $pass = 'e2e15cf7';
+    $db = 'heroku_700de64fb6a62d3';
+    $conn = new mysqli($host,$user,$pass,$db);
+    
+    mysqli_query($conn, "SET NAMES utf8");
 
     $office = $_POST["office"];
     $keyword = $_POST["keyword"];
