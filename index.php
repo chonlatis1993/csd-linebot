f08094064112c36d0e703b6b48e9649814ba410c
chonlatis <chonlatis1993@gmail.com>
2019-05-21 22:03:00 +0700

    A

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <html>
-	<head>
-		
+	<head>		
 	</head>
+    <center>
 	<body>
 		<header style="margin-bottom: 70px;">
 			<nav class="shadow-sm navbar fixed-top navbar-light bg-white">
 				<!-- Brand/logo -->
 				<a class="navbar-brand font-weight-bold" href="#!">
-					<img src="assets/images/pea-logo.png" alt="logo" style="width:100px;">
-					ค้นหาข้อมูลอุปกรณ์
+					<img src="assets/images/pea-logo.png" alt="logo" style="width:1000px;">					
 				</a>
 			</nav>
 		</header>		
 	</body>
+    </center>
 </html>
