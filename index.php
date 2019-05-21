0e4e77c2c142c3a1e92b15439b9cec19fa873b18
chonlatis <chonlatis1993@gmail.com>
2019-05-21 22:04:07 +0700

    A

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 			<nav class="shadow-sm navbar fixed-top navbar-light bg-white">
 				<!-- Brand/logo -->
 				<a class="navbar-brand font-weight-bold" href="#!">
-					<img src="assets/images/pea-logo.png" alt="logo" style="width:1000px;">					
+					<img src="assets/images/pea-logo.png" alt="logo" style="width:800px;">					
 				</a>
 			</nav>
 		</header>		
