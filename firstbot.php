b8a693784c168f3611cee9c6319188dcacdab13d
chonlatis <50080848+chonlatis1993@users.noreply.github.com>
2019-05-28 22:08:19 +0700

    a

--- firstbot.php
+++ firstbot.php
@@ -40,7 +40,7 @@ if (!is_null($events['events'])) //check ค่าในตัวแปร $even
             {
                 $txtback = $txtback."\n".$obj["answer"];
             }
-           reply_msg($txtback,$txtback2,$replyToken);      
+           reply_msg($txtback,$replyToken);      
         }
     }
 }
