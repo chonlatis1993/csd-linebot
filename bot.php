068ab2733a391435f3062fe747c16a50da2cd56b
chonlatis <50080848+chonlatis1993@users.noreply.github.com>
2019-05-30 18:21:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,11 +1,9 @@
 <?php
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('W5b6+6G5LSrSSdceLywdyXBxJJkMblfAtmTzQXPvzmx31jtc75WYnglsAEQyfa5m+yZDHBYBb8ZJFlx6mpE052KqMKEAREJIrUVb/RtI3A1+YdkMBROgwcP2Bg13kGaxF3BUlnd6AT8m76Z7CDkHCgdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['beca049c4ff65cbfde572cd096c1a7fe
-' => 'beca049c4ff65cbfde572cd096c1a7fe
-']);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'beca049c4ff65cbfde572cd096c1a7fe']);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage('W5b6+6G5LSrSSdceLywdyXBxJJkMblfAtmTzQXPvzmx31jtc75WYnglsAEQyfa5m+yZDHBYBb8ZJFlx6mpE052KqMKEAREJIrUVb/RtI3A1+YdkMBROgwcP2Bg13kGaxF3BUlnd6AT8m76Z7CDkHCgdB04t89/1O/w1cDnyilFU=', $textMessageBuilder);
+$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
